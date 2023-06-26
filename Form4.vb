a5690c3274879cf094be219ca2c5cbeb2a4d3499
Sudipta <sudiptabkhs4@gmail.com>
2023-06-26 15:23:34 +0600

    uhj

--- Form4.vb
+++ Form4.vb
@@ -147,4 +147,8 @@ Public Class Form4
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
     End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
 End Class
