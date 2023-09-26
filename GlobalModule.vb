af329419185460b20f4c203d169a3734cb63db6b
Sudipta <sudiptabkhs4@gmail.com>
2023-09-26 11:13:28 +0600

    Global Module added

---
+++ GlobalModule.vb
@@ -0,0 +1,73 @@
+﻿Module GlobalModule
+    Public Variable1 As String
+    Public Variable2 As String
+    Public Header As Boolean
+    Public Ascending As Boolean
+    Public Descending As Boolean
+    Public TextConvert As Boolean
+    Public OptionType As Boolean
+    Public Horizontal_CreateDP As Boolean
+    Public Flag_CreateDDDL As Boolean
+    Public sheetName As String
+
+    'For form-34
+    Public Flag_Picture As Boolean
+    Public sheetName2 As String
+    Public Src_Rng_of_PictureDDL As String
+    Public Des_Rng_of_PictureDDL As String
+
+    'Form35-36
+    Public GB_CB_Source1 As String
+    Public SR1 As String
+    Public Horizontal1 As Boolean
+    Public Separator1 As String
+    Public Search1 As Boolean
+    Public Flag1 As Boolean
+    Public TargetVar1 As String
+    Public RangeType1 As String
+    Public shName1 As String
+    'new
+    Public Nam1 As String    'For multi-selected drop-down
+    Public Nam2 As String    'For multi-selected with check-box
+    Public Nam3 As String    'For drop-down with search
+
+    'Form37-38
+    Public GB_CB_Source2 As String
+    Public SR2 As String
+    Public Horizontal2 As Boolean
+    Public Separator2 As String
+    Public Search2 As Boolean
+    Public Flag2 As Boolean
+    Public TargetVar2 As String
+    Public RangeType2 As String
+    Public shName2 As String
+
+    'Form39-40
+    Public GB_CB_Source3 As String
+    Public SR3 As String
+    Public Horizontal3 As Boolean
+    Public Separator3 As String
+    Public Search3 As Boolean
+    Public Flag3 As Boolean
+    Public TargetVar3 As String
+    Public RangeType3 As String
+    Public shName3 As String
+
+
+    'Form 41
+    Public TType1 As String             'For multi-selected drop-down delete
+    Public GB_CB_Dlt1 As String
+
+    Public TType2 As String             'Fpr multi-selected with checkbox delete
+    Public GB_CB_Dlt2 As String
+
+    Public TType3 As String             'For drop-down with searchbox delete 
+    Public GB_CB_Dlt3 As String
+
+
+    Public settingflag1 As Boolean
+    Public settingflag2 As Boolean
+    Public settingflag3 As Boolean
+
+
+End Module
